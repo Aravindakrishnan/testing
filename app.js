9897cd4c7af505918c27b14f71b4963d34e3b061
Aravindakrishnan <aravindapks@gmail.com>
2020-08-07 11:34:30 +0530

    Added Features

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
-let todos = [
-  {
-    id: 1,
-    title: "Task 1",
-    desc: "Wake up @5:00AM",
-  },
-  {
-    id: 2,
-    title: "Task 1",
-    desc: "Wake up @5:00AM is second",
-  },
-  {
-    id: 3,
-    title: "Task 1",
-    desc: "Wake up @5:00AM is third",
-  },
-];
+let inputdata = document.getElementById("inputdata");
+let alertbox = document.getElementById("alertbox");
 
-console.log(JSON.stringify(todos));
-let data = document.getElementById("tag");
-data.innerHTML = JSON.stringify(todos);
+let btn = document.getElementById("btn");
+
+let dangertemplate = `<div class="alert danger">
+You Have a Dangerous Bug in your system 🐛
+</div>`;
+
+let primarytemplate = `<div class="alert primary">
+You Have a Primary Bug in your system 😅
+</div>`;
+
+let successtemplate = `<div class="alert success">
+You Have a Success Bug in your system 🍏
+</div>`;
+
+let warningtemplate = `
+<div class="alert warning">
+You Have a Warning Bug in your system 💛
+</div>`;
+
+let nfoundtemplate = `
+<div class="alert nfound">
+404 Not Found Template 😆
+</div>`;
+
+btn.addEventListener("click", () => {
+  let check = inputdata.value;
+  if (check == "primary") {
+    alertbox.innerHTML = primarytemplate;
+  } else if (check == "success") {
+    alertbox.innerHTML = successtemplate;
+  } else if (check == "danger") {
+    alertbox.innerHTML = dangertemplate;
+  } else if (check == "warning") {
+    alertbox.innerHTML = warningtemplate;
+  } else {
+    alertbox.innerHTML = nfoundtemplate;
+  }
+  inputdata.value = "";
+});
